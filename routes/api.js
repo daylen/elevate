a22206805844d9a449c5c479bb9b9778636c4d0c
Daylen Yang <services@daylenyang.com>
2015-07-28 00:11:25 -0700

    Return Fitbit data at activity endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/summary', function(req, res, next) {
-  res.json({
-  	'totals': {},
-  	'activities': []
-  });
+var Fitbit = require('../model/fitbit');
+var Strava = require('../model/strava');
+var _ = require('underscore');
+
+router.get('/activity', function(req, res, next) {
+	if (!req.query.date) return next(new Error('date param is required'));
+	var dateObj = new Date(req.query.date);
+
+	Fitbit.findOne({dateTime: dateObj}, function(err, data) {
+		if (err) return next(err);
+		res.json({
+			'date': req.query.date,
+			'summary': _.pick(data, globalConfig.fitbit.exposed),
+			'workouts': []
+		});
+	});
+	
 });
 
 module.exports = router;
