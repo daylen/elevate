ef168b8ed78af5aeaa295970cb0f36ad148eaedc
Daylen Yang <services@daylenyang.com>
2015-08-02 23:38:22 -0700

    New endpoint for batch fetching

--- routes/api.js
+++ routes/api.js
@@ -8,14 +8,58 @@ var _ = require('underscore');
 var async = require('async');
 var logger = require('../logger');
 var moment = require('moment');
+var querystring = require('querystring');
 
 router.get('/activity', function(req, res, next) {
-	if (!req.query.date)
-		return next(new Error('date param is required'));
-	if (isNaN(Date.parse(req.query.date)))
-		return next(new Error('could not parse date'));
-	var dateObj = new Date(req.query.date);
+	var fromStr;
+	var toStr;
+	if (req.query.from && req.query.to) {
+		fromStr = req.query.from;
+		toStr = req.query.to;
+	} else {
+		// Use default date range
+		fromStr = moment().subtract(2, 'weeks').format('YYYY-MM-DD');
+		toStr = moment().format('YYYY-MM-DD');
+	}
+	// Generate all date strings
+	var dateStrs = [fromStr];
+	while (moment(_.last(dateStrs)).isBefore(toStr)) {
+		dateStrs.push(moment(_.last(dateStrs)).add(1, 'day').format('YYYY-MM-DD'));
+	}
+	// Calculate pagination url
+	var url = '/api/v1/activity?';
+	var newToStr = moment(fromStr).subtract(1, 'day').format('YYYY-MM-DD');
+	var newFromStr = moment(newToStr).subtract(2, 'weeks').format('YYYY-MM-DD');
+	var paginationParams = {
+		from: newFromStr,
+		to: newToStr
+	};
+	url += querystring.stringify(paginationParams);
 
+	// Parallel generate
+	async.map(dateStrs, _genResponseForDate, function(err, results) {
+		if (err) return next(err);
+		res.json({
+			activities: results,
+			next: url
+		});
+	});
+
+});
+
+router.get('/activity/:date', function(req, res, next) {
+	if (!req.params.date)
+		return next(new Error('date param is required'));
+	if (isNaN(Date.parse(req.params.date)))
+		return next(new Error('could not parse date'));
+	_genResponseForDate(req.params.date, function(err, json) {
+		if (err) return next(err);
+		res.json(json);
+	});
+});
+
+function _genResponseForDate(dateStr, callback) {
+	var dateObj = new Date(dateStr);
 	async.parallel([
 		function(callback) {
 			_genSummaryForDate(dateObj, function(err, summary) {
@@ -24,30 +68,28 @@ router.get('/activity', function(req, res, next) {
 			});
 		},
 		function(callback) {
-			_genActivitiesForDate(req.query.date, function(err, activities) {
+			_genActivitiesForDate(dateStr, function(err, activities) {
 				if (err) return callback(err);
 				callback(null, activities);
 			});
 		}
 	], function(err, results) {
-		if (err) return next(err);
-		res.json({
-			date: req.query.date,
+		if (err) return callback(err);
+		callback(null, {
+			date: dateStr,
 			summary: results[0],
 			activities: results[1]
 		});
 	});
-});
+}
 
 function _genActivitiesForDate(dateStr, callback) {
-	logger.info('Generating activities for ' + dateStr);
 	var startDate = moment(dateStr);
 	var endDate = moment(dateStr).add(1, 'day');
 	Strava.find({
 		start_date_local: {$gte: startDate, $lt: endDate}
 	}, function(err, activities) {
 		if (err) return callback(err);
-		logger.info('Found ' + activities.length + ' activities');
 		var activityObjects = _.map(activities, function(a) {
 			var b = a.toObject();
 			b = _.omit(b, 'start_latlng', 'end_latlng', '_id', '__v', 'external_id', 'has_kudoed', 'total_photo_count', 'gear_id', 'photo_count', 'start_longitude', 'start_latitude', 'upload_id', 'resource_state');
