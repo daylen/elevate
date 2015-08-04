dde81658a222b4447afe9244fadf5042882a21ed
Daylen Yang <services@daylenyang.com>
2015-08-04 00:29:18 -0700

    Improved day list appearance

--- public/jsx/app.jsx
+++ public/jsx/app.jsx
@@ -2,6 +2,7 @@
 var Day = Backbone.Model.extend({
 	idAttribute: 'date',
 });
+
 var DayCollection = Backbone.Collection.extend({
 	model: Day,
 	url: '/api/v1/activity',
@@ -22,11 +23,7 @@ dayCollection.fetch().then(function() {
 	console.log(dayCollection.length + ' days');
 });
 
-// Util
-function _optVal(val) {
-	return val ? val : '--';
-}
-
+// Utils
 function _metersToMiles(meters) {
 	return meters / 1609.344;
 }
@@ -34,50 +31,79 @@ function _metersToMiles(meters) {
 // React
 var Toolbar = React.createClass({
 	render: function() {
-		return <h1>Elevate</h1>;
+		return <div className="toolbar">
+			<div className="content">
+				<h1>elevate</h1>
+			</div>
+		</div>;
 	},
 });
+
 var DayCellList = React.createClass({
 	mixins: [Backbone.React.Component.mixin],
 	createDay: function(day) {
 		var dateStr = moment(day.date).format('MMM D');
+		var weekdayStr = moment(day.date).format('ddd');
 		var metersBiked = _.reduce(_.filter(day.activities, function(x) {
 			return x.type === "Ride";
 		}), function(acc, x) {
 			return acc + x.distance;
 		}, 0);
 		return <DayCell date={dateStr}
-			steps={_optVal(day.summary.steps)}
-			calories={_optVal(day.summary.calories)}
-			floors={_optVal(day.summary.floors)}
+			weekday={weekdayStr}
+			steps={day.summary.steps || 0}
+			calories={day.summary.calories || 0}
+			floors={day.summary.floors || 0}
 			heart={day.summary.heart ? day.summary.heart.restingHeartRate : '--'}
-			activeTime={_optVal(day.summary.activeTime)}
-			milesBiked={day.activities.length > 0 ? _metersToMiles(metersBiked).toFixed(1) : '--'} />
+			activeTime={day.summary.activeTime || 0}
+			milesBiked={day.activities.length > 0 ? _metersToMiles(metersBiked).toFixed(1) : 0} />
 	},
 	render: function() {
 		return (
-			<div className="dayCellList">
+			<div className="content">
 				{this.state.collection.map(this.createDay)}
 			</div>
 		)
 	},
 });
-var DayCell = React.createClass({
 
+var DayCell = React.createClass({
 	render: function() {
 		return (
-			<div className="dayCell">
-				<div><b>{this.props.date}</b></div>
-				<div>{this.props.steps} steps</div>
-				<div>{this.props.calories} calories</div>
-				<div>{this.props.floors} floors</div>
-				<div>{this.props.heart} bpm resting</div>
-				<div>{this.props.activeTime} active minutes</div>
-				<div>{this.props.milesBiked} miles biked</div>
+			<div className="day">
+				<div className="nugget">
+					<div className="measurement">{this.props.date}</div>
+					<div className="label">{this.props.weekday}</div>
+				</div>
+				<div className="nugget">
+					<div className="measurement">{this.props.steps}</div>
+					<div className="label">steps</div>
+				</div>
+				<div className="nugget">
+					<div className="measurement">{this.props.calories}</div>
+					<div className="label">calories burned</div>
+				</div>
+				<div className="nugget">
+					<div className="measurement">{this.props.floors}</div>
+					<div className="label">floors</div>
+				</div>
+				<div className="nugget">
+					<div className="measurement">{this.props.heart}</div>
+					<div className="label">bpm resting</div>
+				</div>
+				<div className="nugget">
+					<div className="measurement">{this.props.activeTime}</div>
+					<div className="label">active minutes</div>
+				</div>
+				<div className="nugget">
+					<div className="measurement">{this.props.milesBiked}</div>
+					<div className="label">miles biked</div>
+				</div>
 			</div>
 		);
 	},
 });
+
 React.render((
 	<div>
 		<Toolbar />
